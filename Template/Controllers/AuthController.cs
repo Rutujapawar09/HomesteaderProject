e0741220bf4fdbfdaf694176411284eb70293c1f
Gayatri4199 <toradmalgayatri@gmail.com>
2026-01-02 11:14:55 +0530

    Login and registration age

---
+++ Template/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
