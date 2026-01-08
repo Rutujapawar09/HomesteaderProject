c7d8619e8d4d70166d2260ed333c15b72bcd4587
Rutuja <Rutuja@LAPTOP-8665JUUD>
2026-01-08 15:41:48 +0530

    worked on Farmer Dashboard

--- Template/Controllers/AuthController.cs
+++ Template/Controllers/AuthController.cs
@@ -86,5 +86,6 @@ namespace Template.Controllers
 
             return RedirectToAction("Login");
         }
+
     }
 }
---
+++ Template/Controllers/FarmerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Controllers
+{
+    public class FarmerController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            // Optional security check
+            if (HttpContext.Session.GetString("UserRole") != "Farmer")
+                return RedirectToAction("Login", "Auth");
+
+            return View();
+        }
+    }
+}
